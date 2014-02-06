175fc920948fff24e68e4234bbd3f122e36a2a83
Masahiro Nagano <kazeburo@gmail.com>
2014-02-07 03:08:04 +0900

    benchmark

--- eg/bench.pl
+++ eg/bench.pl
@@ -4,8 +4,10 @@ use strict;
 use warnings;
 use Benchmark qw/:all/;
 use WWW::Form::UrlEncoded;
+use WWW::Form::UrlEncoded::PP;
 use URL::Encode;
 use Text::QueryString;
+
 
 my @query_string = (
     "foo=bar",
@@ -15,8 +17,10 @@ my @query_string = (
     "foo=bar&foo=baz&bar=baz",
     "foo_only",
     "foo&bar=baz",
-    "日本語=にほんご&ほげほげ=1&ふがふが",
+    '%E6%97%A5%E6%9C%AC%E8%AA%9E=%E3%81%AB%E3%81%BB%E3%82%93%E3%81%94&%E3%81%BB%E3%81%92%E3%81%BB%E3%81%92=%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C',
 );
+
+
 my $xs = Text::QueryString->new;
 
 cmpthese(-1, {
@@ -28,6 +32,11 @@ cmpthese(-1, {
     wwwform => sub {
         foreach my $qs (@query_string) {
             my @q = WWW::Form::UrlEncoded::parse_urlencoded($qs);
+        }
+    },
+    wwwform_pp => sub {
+        foreach my $qs (@query_string) {
+            my @q = WWW::Form::UrlEncoded::PP::parse_urlencoded($qs);
         }
     },
     urlecode => sub {
