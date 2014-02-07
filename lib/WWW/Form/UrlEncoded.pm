7eabb9eefde1b85cf18f5645793c9a091068a9ec
Masahiro Nagano <kazeburo@gmail.com>
2014-02-07 13:30:42 +0900

    write pod

--- lib/WWW/Form/UrlEncoded.pm
+++ lib/WWW/Form/UrlEncoded.pm
@@ -36,15 +36,92 @@ __END__
 
 =head1 NAME
 
-WWW::Form::UrlEncoded - It's new $module
+WWW::Form::UrlEncoded - parser for application/x-www-form-urlencoded
 
 =head1 SYNOPSIS
 
-    use WWW::Form::UrlEncoded;
+    use WWW::Form::UrlEncoded qw/parse_urlencoded/;
+
+    my $query_string = "foo=bar&baz=param";
+    my @params = parse_urlencoded($query_string);
+    # ('foo','bar','baz','param')
 
 =head1 DESCRIPTION
 
-WWW::Form::UrlEncoded is ...
+WWW::Form::UrlEncoded provides application/x-www-form-urlencoded parser.
+This module aims to have compatibility with other CPAN modules like HTTP::Body
+and be fast by XS implementation.
+
+=head2 Parser rules
+
+WWW::Form::UrlEncoded parsed string in this rule.
+
+=over 4
+
+=item 1. Split application/x-www-form-urlencoded payload by C<&> (U+0026) or C<;> (U+003B)
+
+=item 2. Ready empty array to store C<name> and C<value>
+
+=item 3. For each divided string, apply next steps.
+
+=over 4
+
+=item 1. If first character of string is C<' '> (U+0020 SPACE), remove it.
+
+=item 2. If string has C<=>, let B<name> be substring from start to first C<=>, but excluding first C<=>, and remains to be B<value>. If there is no strings after first C<=>, B<value> to be empty string C<"">. If first C<=> is first character of the string, let B<key> be empty string C<"">. If string does not have any C<=>, all of the string to be B<key> and B<value> to be empty string C<"">.
+
+=item 3. replace all C<+> (U+002B) with C<' '> (U+0020 SPACE).
+
+=item 4. unescape B<name> and B<value>. push them to the array.
+
+=back
+
+=item 4. return the array.
+
+=back
+
+=head2 Test data
+
+  'a=b&c=d'     => ["a","b","c","d"]
+  'a=b;c=d'     => ["a","b","c","d"]
+  'a=1&b=2;c=3' => ["a","1","b","2","c","3"]
+  'a==b&c==d'   => ["a","=b","c","=d"]
+  'a=b& c=d'    => ["a","b","c","d"]
+  'a=b; c=d'    => ["a","b","c","d"]
+  'a=b; c =d'   => ["a","b","c ","d"]
+  'a=b;c= d '   => ["a","b","c"," d "]
+  'a=b&+c=d'    => ["a","b"," c","d"]
+  'a=b&+c+=d'   => ["a","b"," c ","d"]
+  'a=b&c=+d+'   => ["a","b","c"," d "]
+  'a=b&%20c=d'  => ["a","b"," c","d"]
+  'a=b&%20c%20=d' => ["a","b"," c ","d"]
+  'a=b&c=%20d%20' => ["a","b","c"," d "]
+  'a&c=d'       => ["a","","c","d"]
+  'a=b&=d'      => ["a","b","","d"]
+  'a=b&='       => ["a","b","",""]
+  '&'           => ["","","",""]
+  '='           => ["",""]
+  ''            => []
+
+=head1 FUNCTION
+
+=over 4
+
+=item @param = parse_urlencoded($str:String)
+
+parse C<$str> and return Array that contains key-value pairs.
+
+=back
+
+=head1 ENVIRONMENT VALUE
+
+=over 4
+
+=item * WWW_FORM_URLENCODED_PP
+
+If true, WWW::Form::UrlEncoded force to use WWW::Form::UrlEncoded::PP.
+
+=back
 
 =head1 LICENSE
 
