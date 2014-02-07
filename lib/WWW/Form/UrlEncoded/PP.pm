e19344261cea28cb91acc61b898cd4796cad8838
Masahiro Nagano <kazeburo@gmail.com>
2014-02-07 13:34:55 +0900

    add pod

--- lib/WWW/Form/UrlEncoded/PP.pm
+++ lib/WWW/Form/UrlEncoded/PP.pm
@@ -38,3 +38,37 @@ sub parse_urlencoded {
 
 1;
 
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Form::UrlEncoded::PP - pure-perl parser for application/x-www-form-urlencoded
+
+=head1 SYNOPSIS
+
+    use WWW::Form::UrlEncoded::PP qw/parse_urlencoded/;
+
+    my $query_string = "foo=bar&baz=param";
+    my @params = parse_urlencoded($query_string);
+    # ('foo','bar','baz','param')
+
+=head1 DESCRIPTION
+
+WWW::Form::UrlEncoded::PP provides pure-perl application/x-www-form-urlencoded parser.
+see L<WWW::Form::UrlEncoded>'s document.
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Nagano.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo@gmail.comE<gt>
+
+=cut
+
--- lib/WWW/Form/UrlEncoded/XS.pm
+++ lib/WWW/Form/UrlEncoded/XS.pm
@@ -12,3 +12,37 @@ XSLoader::load(__PACKAGE__, $VERSION);
 
 1;
 
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Form::UrlEncoded::XS - XS implementation of parser for application/x-www-form-urlencoded
+
+=head1 SYNOPSIS
+
+    use WWW::Form::UrlEncoded::XS qw/parse_urlencoded/;
+
+    my $query_string = "foo=bar&baz=param";
+    my @params = parse_urlencoded($query_string);
+    # ('foo','bar','baz','param')
+
+=head1 DESCRIPTION
+
+WWW::Form::UrlEncoded::XS provides application/x-www-form-urlencoded parser that implementated by XS.
+see L<WWW::Form::UrlEncoded>'s document.
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Nagano.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo@gmail.comE<gt>
+
+=cut
+
