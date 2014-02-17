0ca61ac9e0264092ab3f1c90cdfb3b81763f7a49
Masahiro Nagano <kazeburo@gmail.com>
2014-02-17 14:55:24 +0900

    split XS into another dist

--- lib/WWW/Form/UrlEncoded.pm
+++ lib/WWW/Form/UrlEncoded.pm
@@ -53,7 +53,10 @@ WWW::Form::UrlEncoded - parser and builder for application/x-www-form-urlencoded
 
 WWW::Form::UrlEncoded provides application/x-www-form-urlencoded parser and builder.
 This module aims to have compatibility with other CPAN modules like 
-HTTP::Body's urlencoded parser. And aims be fast by XS implementation.
+HTTP::Body's urlencoded parser.
+
+This module try to use L<WWW::Form::UrlEncoded::XS> by default and fail to it, 
+use WWW::Form::UrlEncoded::PP instead
 
 =head2 Parser rules
 
